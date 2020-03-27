79fbf82fe255fe7332742f486f41c753a5c487aa
Sander Leeffers <s.leeffers@agrifirm.com>
2020-03-27 16:48:12 +0100

    build and deploy

---
+++ build/precache-manifest.9a83addc52b95b56f58b281700060683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa051dcc2c354fc016f25f131123bde6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97fe6ff4f0120624250f",
+    "url": "/static/css/main.a5f71f74.chunk.css"
+  },
+  {
+    "revision": "74352d00a2eb365c0e7a",
+    "url": "/static/js/2.ae0063d5.chunk.js"
+  },
+  {
+    "revision": "b68ea9bfcdfe88fd25eac9df29279526",
+    "url": "/static/js/2.ae0063d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fe6ff4f0120624250f",
+    "url": "/static/js/main.60b1dcaf.chunk.js"
+  },
+  {
+    "revision": "85d0d54b4ee41a1e9bb4",
+    "url": "/static/js/runtime-main.27509736.js"
+  }
+]);

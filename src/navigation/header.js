fac90698dca84e41a7eb4747554e1345083e4b9f
Sander Leeffers <s.leeffers@agrifirm.com>
2020-04-01 16:36:22 +0200

    highlight on active

--- src/navigation/header.js
+++ src/navigation/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, darken } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -14,6 +14,9 @@ const useStyles = makeStyles(theme => ({
     },
     menuButton: {
         marginRight: theme.spacing(2),
+    },
+    highlight: {
+        backgroundColor: darken("rgba(103, 213, 24)", 0.1),
     },
     title: {
         flexGrow: 1,
@@ -30,18 +33,34 @@ export default function Header({ profile }) {
                     <Typography variant="h6" className={classes.title}>
                         CODEZILLA Hours
                     </Typography>
-                    <Button component={NavLink} color="inherit" to="/">
+                    <Button
+                        component={NavLink}
+                        color="inherit"
+                        to="/"
+                        exact
+                        activeClassName={classes.highlight}
+                    >
                         Urenstaat
                     </Button>
-                    <Button component={NavLink} color="inherit" to="/template">
+                    <Button
+                        component={NavLink}
+                        color="inherit"
+                        to="/template"
+                        activeClassName={classes.highlight}
+                    >
                         Template
                     </Button>
-                    <Button
-                        color="inherit"
-                        component={NavLink}
-                        to="/admin"
-                        startIcon={<AccountCircle />}
-                    >
+                    {profile.isAdmin && (
+                        <Button
+                            component={NavLink}
+                            color="inherit"
+                            to="/admin"
+                            activeClassName={classes.highlight}
+                        >
+                            Beheer
+                        </Button>
+                    )}
+                    <Button color="inherit" startIcon={<AccountCircle />}>
                         {profile.displayName}
                     </Button>
                 </Toolbar>
